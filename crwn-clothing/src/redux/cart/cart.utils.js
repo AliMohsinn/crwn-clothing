72a7ad23c869f6006e3820aa76031f90c83bb24d
veeer <veeer@coursera.com>
2020-02-19 13:18:28 +0500

    added remove item and add item functionality to checkout item component

--- crwn-clothing/src/redux/cart/cart.utils.js
+++ crwn-clothing/src/redux/cart/cart.utils.js
@@ -1,3 +1,25 @@
+export const clearItemFromCart =(cartItems, cartItemToRemove) => {
+  return cartItems.filter(
+    cartItem =>  cartItem.id !== cartItemToRemove.id
+  )
+
+};
+
+export const removeItemFromCart =(cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+  if(existingCartItem.quantity==1)
+  {
+    return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id)
+  }
+
+  return cartItems.map(
+    cartItem => 
+    cartItem.id==cartItemToRemove.id ?
+    { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+  );
+};
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
       cartItem => cartItem.id === cartItemToAdd.id
@@ -5,11 +27,14 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   
     if (existingCartItem) {
       return cartItems.map(cartItem =>
-        cartItem.id === cartItemToAdd.id
+        cartItem.id == cartItemToAdd.id
           ? { ...cartItem, quantity: cartItem.quantity + 1 }
           : cartItem
       );
     }
   
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-  };
+
+};
+
+ 
